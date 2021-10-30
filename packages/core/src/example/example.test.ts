943a4563800062354f76ef01db3f9afa4d84d0a3
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-10-30 22:21:18 +0900

    feat: nnn

--- packages/core/src/example/example.test.ts
+++ packages/core/src/example/example.test.ts
@@ -3,5 +3,9 @@ import { example } from "./example";
 describe("example", () => {
   test("動作確認", () => {
     expect(example()).toBeDefined();
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    expect(example("")).toBeDefined();
   });
 });
